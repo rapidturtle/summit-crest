1ca399de465c85381e96ac312b1be611698661d9
Andrew Porter <aporter@rapidturtle.com>
2012-04-24 16:28:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummitCrest::Application.config.secret_token = '1eda302bba08cbb914caff0bde853adf4b9846f2a936d23be946a5b0d59385b3cad479c2ead564959796b7201baf2c4c64defb01e4179317d580061d5881cdd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SummitCrest::Application.config.session_store :cookie_store, key: '_summit_crest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SummitCrest::Application.config.session_store :active_record_store
