cc68c684feebffb84ab0f23d1deb94c15fe04a30
Andrew Porter <aporter@rapidturtle.com>
2015-05-15 10:54:08 -0600

    Add about, contact, and support pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,4 +5,19 @@ describe StaticPagesController do
     get :home
     must_respond_with :success
   end
+
+  it "gets about" do
+    get :about
+    must_respond_with :success
+  end
+
+  it "gets contact" do
+    get :contact
+    must_respond_with :success
+  end
+
+  it "gets support" do
+    get :support
+    must_respond_with :success
+  end
 end
